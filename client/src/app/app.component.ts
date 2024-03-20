4f5b1838f19fbc63e1500a7496ef875f47dfec95
Kyle Plummer <kyle.plummer@revature.com>
2024-03-20 16:47:39 -0400

    completed solution

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -14,11 +14,11 @@ export class AppComponent {
   title = 'client';
   httpClient: HttpClient;
   file: File | undefined;
+  downloadFile: Blob | undefined;
 
   constructor(httpClient: HttpClient) {
     /* the HttpClient needs to be provided, see app.config.ts */
     this.httpClient = httpClient;
-    this.file = undefined;
   }
 
   fileSelected(event: any) {
@@ -42,19 +42,29 @@ export class AppComponent {
       }
     });
     response.subscribe({
-      next: (data) => {console.log("data: ", data)},
+      next: (data) => {
+        console.log("data: ", data);
+        let fileName: string | null = "file.txt";
+        let fileBody: string | null = data.body;
+        this.downloadFile = new Blob([fileBody as string], {type: "text/plain"});
+      },
       error: (error: HttpErrorResponse) => {
-        console.log("error: ", error)
+        console.log("error: ", error);
+        alert(error.message);
       }
     });
   }
 
   saveFile() {
-    let fileName = "test.file";
-    let fileBody = "This is a test";
-
-    const file = new Blob([fileBody], {type: "text/plain"});
+    if(this.downloadFile == undefined) {
+      alert("No file to download!");
+      return;
+    }
 
     const linkElement = document.createElement("a");
+    linkElement.href = URL.createObjectURL(this.downloadFile);
+    linkElement.download = "download.file";
+    linkElement.click();
+    linkElement.remove();
   }
 }
